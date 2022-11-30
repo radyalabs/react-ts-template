a3fc3e6c042853c9bb49dac3c4e3bc957ee222e0
Hasbi Ashshidiq <ashshidiq23@gmail.com>
2022-11-30 22:37:15 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      'files': ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      'rules': {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            'groups': [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$']
+            ]
+          }
+        ]
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-simple-import-sort'
+  ],
+  'rules': {
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'warn'
+  }
+};
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes} from 'react-router-dom';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <React.Suspense fallback={<div>Loading</div>}>
+        <Routes>
+        </Routes>
+      </React.Suspense>
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
