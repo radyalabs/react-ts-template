19938f88b357682e43476e318aa520347727902f
Hasbi Ashshidiq <ashshidiq23@gmail.com>
2023-01-19 12:09:24 +0700

    feat: setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary-500': '#1fb6ff',
+      'primary-200': '#1fffff',
+      'secondary-500': '#ff7849',
+      'secondary-200': '#ffff49',
+      'accent-500': '#ffc82c',
+      'accent-200': '#ffff2c',
+      'variant-500': '#8492a6',
+      'variant-200': '#dddddd',
+    },
+    fontFamily: {
+      sans: [],
+      serif: [],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 };
